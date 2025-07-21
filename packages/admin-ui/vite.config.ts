2096f7b7e66e6013a10fcd437686955d15f102a1
Ali <angache@hotmail.com>
2025-07-22 01:54:50 +0300

    fix: add environment variables to admin-ui vite config

--- packages/admin-ui/vite.config.ts
+++ packages/admin-ui/vite.config.ts
@@ -26,6 +26,10 @@ export default defineConfig({
     sourcemap: true,
   },
   define: {
-    'process.env': {},
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+      VITE_API_URL: JSON.stringify(`http://${VPS_IP}:3002`),
+      VITE_ELASTICSEARCH_URL: JSON.stringify(`http://${VPS_IP}:3002/api/v1/elasticsearch`),
+    },
   },
 })
